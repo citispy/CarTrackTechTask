09a62b29843ad9573488e7ac18ef705e0f4fe1b6
Yusuf <yusuf.isaacs@rapiddeploy.com>
2021-12-29 10:09:24 +0200

    Rename ViewModel

--- app/src/main/java/com/rapiddeploy/mobile/cartracktechtask/ui/search/SearchFragment.kt
+++ app/src/main/java/com/rapiddeploy/mobile/cartracktechtask/ui/search/SearchFragment.kt
@@ -10,13 +10,14 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.rapiddeploy.mobile.cartracktechtask.R
+import com.rapiddeploy.mobile.cartracktechtask.api.model.Title
 import com.rapiddeploy.mobile.cartracktechtask.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
 
-    private val searchViewModel: SearchViewModel by viewModels()
+    private val titlesViewModel: TitlesViewModel by viewModels()
     private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
@@ -27,10 +28,10 @@ class SearchFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
 
         binding.button.setOnClickListener {
-            searchViewModel.getTitles()
+            titlesViewModel.getTitles("good", Title.Type.MOVIE)
         }
 
-        searchViewModel.titles.observe(this, { titles ->
+        titlesViewModel.titles.observe(this, { titles ->
             var titleString: String? = null
             if (titles != null) {
                 for (title in titles) {
--- app/src/main/java/com/rapiddeploy/mobile/cartracktechtask/ui/search/TitlesViewModel.kt
+++ app/src/main/java/com/rapiddeploy/mobile/cartracktechtask/ui/search/TitlesViewModel.kt
@@ -6,11 +6,11 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel @Inject constructor(private val repository: TitlesRepository) : ViewModel() {
+class TitlesViewModel @Inject constructor(private val repository: TitlesRepository) : ViewModel() {
 
     val titles = repository.titles
 
-    fun getTitles() {
-        repository.getTitles("good", Title.Type.MOVIE)
+    fun getTitles(searchTitle: String, type: Title.Type) {
+        repository.getTitles(searchTitle, type)
     }
 }
