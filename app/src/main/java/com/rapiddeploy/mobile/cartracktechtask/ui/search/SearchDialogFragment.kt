15dd4cf331d5e9c7d850ce735d4b61a0c33ee921
Yusuf <yusuf.isaacs@rapiddeploy.com>
2021-12-29 16:43:22 +0200

    Implement titles searching

---
+++ app/src/main/java/com/rapiddeploy/mobile/cartracktechtask/ui/search/SearchDialogFragment.kt
@@ -0,0 +1,59 @@
+package com.rapiddeploy.mobile.cartracktechtask.ui.search
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.rapiddeploy.mobile.cartracktechtask.R
+import com.rapiddeploy.mobile.cartracktechtask.api.model.Title
+import com.rapiddeploy.mobile.cartracktechtask.databinding.FragmentSearchDialogBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchDialogFragment : DialogFragment() {
+
+    private val titlesViewModel: TitlesViewModel by viewModels()
+    private lateinit var binding: FragmentSearchDialogBinding
+    private var checkedItemType: Title.Type = Title.Type.MOVIE
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(requireContext());
+        builder.setTitle(getString(R.string.search_for_title))
+        builder.setPositiveButton(getString(R.string.search)) { _, _ ->
+                val searchTitle = binding.searchTitle.text.toString()
+                if (searchTitle.isNotEmpty()) {
+                    titlesViewModel.getTitles(searchTitle, checkedItemType)
+                }
+            }
+        builder.setNegativeButton(getString(R.string.cancel)) { _, _ ->
+            //Dismiss dialog
+        }
+        val inflater = LayoutInflater.from(requireContext())
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_dialog, null, false)
+        builder.setView(binding.root)
+        setSingleChoiceItems(builder)
+        return builder.create()
+    }
+
+    private fun setSingleChoiceItems(builder: AlertDialog.Builder) {
+        val alertItems = arrayOf(getString(R.string.movies), getString(R.string.series))
+        builder.setSingleChoiceItems(alertItems, 0) { _: DialogInterface?, checkedItem: Int ->
+            setCheckedItem(alertItems, checkedItem)
+        }
+    }
+
+    private fun setCheckedItem(alertItems: Array<String>, checkedItem: Int) {
+        val checkedItemString = alertItems[checkedItem]
+        if(checkedItemString == getString(R.string.movies)) {
+            checkedItemType = Title.Type.MOVIE
+        } else if (checkedItemString == getString(R.string.series)) {
+            checkedItemType = Title.Type.SERIES
+        }
+    }
+}
--- app/src/main/java/com/rapiddeploy/mobile/cartracktechtask/ui/search/SearchFragment.kt
+++ app/src/main/java/com/rapiddeploy/mobile/cartracktechtask/ui/search/SearchFragment.kt
@@ -1,17 +1,14 @@
 package com.rapiddeploy.mobile.cartracktechtask.ui.search
 
-import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.rapiddeploy.mobile.cartracktechtask.R
 import com.rapiddeploy.mobile.cartracktechtask.api.model.Title
@@ -31,6 +28,9 @@ class SearchFragment : Fragment(), TitleResultsAdapter.OnItemClickListener {
     ): View {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
+        binding.seachFab.setOnClickListener {
+            SearchDialogFragment().show(childFragmentManager, "tag")
+        }
 
         initTitlesList()
         observeViewModel()
@@ -40,27 +40,20 @@ class SearchFragment : Fragment(), TitleResultsAdapter.OnItemClickListener {
 
     private fun initTitlesList() {
         val titlesList: RecyclerView = binding.titlesList
-        titlesList.layoutManager = LinearLayoutManager(requireContext())
+        titlesList.layoutManager = GridLayoutManager(requireContext(), 2)
         titlesList.adapter = adapter
-        titlesViewModel.getTitles("good", Title.Type.MOVIE)
     }
 
     private fun observeViewModel() {
         titlesViewModel.titles.observe(this) {
-            if (it != null) {
-                adapter.updateTitles(it)
+            val moviesFound = it != null
+            if (moviesFound) {
+                adapter.updateTitles(it!!)
             }
+
+            binding.titlesList.visibility = if (moviesFound) View.VISIBLE else View.GONE
+            binding.noMoviesFound.visibility = if (moviesFound) View.GONE else View.VISIBLE
         }
-    }
-
-    private fun showAlert(titles: String) {
-        AlertDialog.Builder(requireActivity()).setTitle("Titles")
-            .setMessage(titles)
-            .setPositiveButton("Okay") { _, _ ->
-
-            }
-            .setCancelable(true)
-            .show()
     }
 
     override fun onItemClicked(title: Title) {
